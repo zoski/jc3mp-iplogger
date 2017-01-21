0d4491497583bffeb1c73fb893b5017a34ec5f9b
SteamCMD <steam@raildecake.fr>
2017-01-21 15:56:24 +0100

    Initial commit ;-)

--- client_package/main.js
+++
@@ -1,2 +0,0 @@
-// And yet again, this is all we need for a client package.
-jcmp.print("This message will appear in the Scripting.log in the 'logs' directory of your client");
--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 // Yes, this is it. This is all you need for a minimal package.
-console.log('Hello there. Obligatory "meow" to make alex happy.');
+console.log('[IPL]: IPlogger started...');
+
+const fs = require('fs');
+
+jcmp.events.Add('ClientConnected', client => {
+    fs.appendFile("logs/IPlogger.log", `${client.ipAddress}\n`, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log(`[IPL] ip : ${client.ipAddress} - steamId : ${client.steamId}`);
+    })
+
+});
