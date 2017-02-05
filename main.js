ecadc4a0a8ee28cbfdab0a3eaa4a3700b5fc8b95
Gaël A <zoski@free.fr>
2017-02-05 16:52:53 +0100

    Updating logging format

--- main.js
+++ main.js
@@ -4,35 +4,37 @@ console.log('[IPL]: IPlogger started...');
 const fs = require('fs');
 
 jcmp.events.Add('ClientConnected', client => {
-var d = new Date();
-//plus 1 to month because js
-var datetime = `${d.getDate()}/${d.getMonth()+1}/${d.getFullYear()} | ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
-    fs.appendFile("logs/IPlogger.log", `JOIN ${datetime} | ip : ${client.ipAddress} - steamId : ${client.steamId} - name : ${client.name}\n`, function(err) {
+    var d = new Date();
+    //plus 1 to month because js
+    var datetime = `${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+
+    fs.appendFile("logs/IPlogger.log", `${datetime} JOIN - name : ${client.name}  - ip : ${client.ipAddress} - steamId : ${client.steamId} - \n`, function(err) {
         if(err) {
             return console.log(err);
         }
 	})
+
+    console.log(`[IPL] JOIN ${datetime} - name : ${client.name} - IP : ${client.ipAddress} - steamId : ${client.steamId}`);
+
     // login only IPs on JOIN
     fs.appendFile("logs/IPs.log", `${client.ipAddress}`, function(err) {
         if(err) {
             return console.log(err);
         }
 	})
-    console.log(`[IPL JOIN] ${datetime} | ip : ${client.ipAddress} - steamId : ${client.steamId} - name : ${client.name}`);
-
-
 });
 
 jcmp.events.Add('ClientDisconnected', client => {
-var d = new Date();
-//plus 1 to month because js
-var datetime = `${d.getDate()}/${d.getMonth()+1}/${d.getFullYear()} | ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
-    fs.appendFile("logs/IPlogger.log", `LEAVE ${datetime} | ip : ${client.ipAddress} - steamId : ${client.steamId} - name : ${client.name}\n`, function(err) {
+    var d = new Date();
+    //plus 1 to month because js
+    var datetime = `${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+
+    console.log("logs/IPlogger.log", `${datetime} LEAVE - name : ${client.name}  - ip : ${client.ipAddress} - steamId : ${client.steamId} - \n`, function(err) {
         if(err) {
             return console.log(err);
         }
 	})
-    console.log(`[IPL LEAVE] ${datetime} | ip : ${client.ipAddress} - steamId : ${client.steamId} - name : ${client.name}`);
+    console.log(`[IPL] LEAVE ${datetime} - name : ${client.name} - IP : ${client.ipAddress} - steamId : ${client.steamId}`);
 
 
 });
